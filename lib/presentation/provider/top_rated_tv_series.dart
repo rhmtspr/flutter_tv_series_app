fec2bc1f4e9356b609548494a83075b205b1846c
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-09 00:36:39 +0800

    create top rated tv series

---
+++ lib/presentation/provider/top_rated_tv_series.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie.dart';
+import 'package:flutter_tv_series_app/domain/entities/tv_series.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_top_rated_movies.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_top_rated_tv_series.dart';
+
+class TopRatedTvSeriesNotifier extends ChangeNotifier {
+  final GetTopRatedTvSeries getTopRatedTvSeries;
+
+  TopRatedTvSeriesNotifier({required this.getTopRatedTvSeries});
+
+  RequestState _state = RequestState.Empty;
+  RequestState get state => _state;
+
+  List<TvSeries> _tvSeries = [];
+  List<TvSeries> get tvSeries => _tvSeries;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchTopRatedTvSeries() async {
+    _state = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getTopRatedTvSeries.execute();
+
+    result.fold(
+      (failure) {
+        _message = failure.message;
+        _state = RequestState.Error;
+        notifyListeners();
+      },
+      (tvSeriesData) {
+        _tvSeries = tvSeriesData;
+        _state = RequestState.Loaded;
+        notifyListeners();
+      },
+    );
+  }
+}
