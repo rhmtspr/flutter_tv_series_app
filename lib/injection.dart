366ed88f21a8130b7f3efd47e3ad7354cdb54356
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-09 00:26:44 +0800

    add tv series provider

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,13 +1,20 @@
 import 'package:flutter_tv_series_app/data/datasources/db/database_helper.dart';
 import 'package:flutter_tv_series_app/data/datasources/movie_local_data_source.dart';
 import 'package:flutter_tv_series_app/data/datasources/movie_remote_data_source.dart';
+import 'package:flutter_tv_series_app/data/datasources/tv_series_local_data_source.dart';
+import 'package:flutter_tv_series_app/data/datasources/tv_series_remote_data_source.dart';
 import 'package:flutter_tv_series_app/data/repositories/movie_repository_impl.dart';
+import 'package:flutter_tv_series_app/data/repositories/tv_series_repository_impl.dart';
 import 'package:flutter_tv_series_app/domain/repositories/movie_repository.dart';
+import 'package:flutter_tv_series_app/domain/repositories/tv_series_repository.dart';
 import 'package:flutter_tv_series_app/domain/usecases/get_movie_detail.dart';
 import 'package:flutter_tv_series_app/domain/usecases/get_movie_recommendations.dart';
 import 'package:flutter_tv_series_app/domain/usecases/get_now_playing_movies.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_now_playing_tv_series.dart';
 import 'package:flutter_tv_series_app/domain/usecases/get_popular_movies.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_popular_tv_series.dart';
 import 'package:flutter_tv_series_app/domain/usecases/get_top_rated_movies.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_top_rated_tv_series.dart';
 import 'package:flutter_tv_series_app/domain/usecases/get_watchlist_movies.dart';
 import 'package:flutter_tv_series_app/domain/usecases/get_watchlist_status.dart';
 import 'package:flutter_tv_series_app/domain/usecases/remove_watchlist.dart';
@@ -17,7 +24,10 @@ import 'package:flutter_tv_series_app/presentation/provider/movie_detail_notifie
 import 'package:flutter_tv_series_app/presentation/provider/movie_list_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/movie_search_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/popular_movies_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/popular_tv_series_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/top_rated_movies_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/top_rated_tv_series.dart';
+import 'package:flutter_tv_series_app/presentation/provider/tv_series_list_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:http/http.dart' as http;
 import 'package:get_it/get_it.dart';
@@ -25,7 +35,7 @@ import 'package:get_it/get_it.dart';
 final locator = GetIt.instance;
 
 void init() {
-  // provider
+  // Movies Provider
   locator.registerFactory(
     () => MovieListNotifier(
       getNowPlayingMovies: locator(),
@@ -51,7 +61,7 @@ void init() {
     () => WatchlistMovieNotifier(getWatchlistMovies: locator()),
   );
 
-  // use case
+  // Movies Use Cases
   locator.registerLazySingleton(() => GetNowPlayingMovies(locator()));
   locator.registerLazySingleton(() => GetPopularMovies(locator()));
   locator.registerLazySingleton(() => GetTopRatedMovies(locator()));
@@ -63,7 +73,7 @@ void init() {
   locator.registerLazySingleton(() => RemoveWatchlist(locator()));
   locator.registerLazySingleton(() => GetWatchlistMovies(locator()));
 
-  // repository
+  // Movie Repository
   locator.registerLazySingleton<MovieRepository>(
     () => MovieRepositoryImpl(
       remoteDataSource: locator(),
@@ -71,12 +81,62 @@ void init() {
     ),
   );
 
-  // data sources
+  // Movie Data Sources
   locator.registerLazySingleton<MovieRemoteDataSource>(
     () => MovieRemoteDataSourceImpl(client: locator()),
   );
   locator.registerLazySingleton<MovieLocalDataSource>(
     () => MovieLocalDataSourceImpl(databaseHelper: locator()),
+  );
+
+  // ======================================
+  // TV SERIES
+  // ======================================
+
+  // Tv Series Provider
+  locator.registerFactory(
+    () => TvSeriesListNotifier(
+      getNowPlayingTvSeries: locator(),
+      getPopularTvSeries: locator(),
+      getTopRatedTvSeries: locator(),
+    ),
+  );
+
+  // locator.registerFactory(() => MovieSearchNotifier(searchMovies: locator()));
+  locator.registerFactory(() => PopularTvSeriesNotifier(locator()));
+  locator.registerFactory(
+    () => TopRatedTvSeriesNotifier(getTopRatedTvSeries: locator()),
+  );
+  // locator.registerFactory(
+  //   () => WatchlistMovieNotifier(getWatchlistMovies: locator()),
+  // );
+
+  // Tv Series Use Cases
+  locator.registerLazySingleton(() => GetNowPlayingTvSeries(locator()));
+  locator.registerLazySingleton(() => GetPopularTvSeries(locator()));
+  locator.registerLazySingleton(() => GetTopRatedTvSeries(locator()));
+  // locator.registerLazySingleton(() => GetTvSeriesDetail(locator()));
+  // locator.registerLazySingleton(() => GetTvSeriesRecommendations(locator()));
+  // locator.registerLazySingleton(() => SearchMovies(locator()));
+  // locator.registerLazySingleton(() => GetWatchListStatus(locator()));
+  // locator.registerLazySingleton(() => SaveWatchlist(locator()));
+  // locator.registerLazySingleton(() => RemoveWatchlist(locator()));
+  // locator.registerLazySingleton(() => GetWatchlistMovies(locator()));
+
+  // Tv Series Repository
+  locator.registerLazySingleton<TvSeriesRepository>(
+    () => TvSeriesRepositoryImpl(
+      remoteDataSource: locator(),
+      localDataSource: locator(),
+    ),
+  );
+
+  // Tv Series Data Sources
+  locator.registerLazySingleton<TvSeriesRemoteDataSource>(
+    () => TvSeriesRemoteDataSourceImpl(client: locator()),
+  );
+  locator.registerLazySingleton<TvSeriesLocalDataSource>(
+    () => TvSeriesLocalDataSourceImpl(databaseHelper: locator()),
   );
 
   // helper
