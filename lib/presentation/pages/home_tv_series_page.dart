4b697fa92b2bf3fe970bb5f9b9d685fb4af337cd
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 21:38:30 +0800

    create home tv series page

---
+++ lib/presentation/pages/home_tv_series_page.dart
@@ -0,0 +1,215 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_tv_series_app/common/constants.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie.dart';
+import 'package:flutter_tv_series_app/domain/entities/tv_series.dart';
+import 'package:flutter_tv_series_app/presentation/pages/about_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/home_movie_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/movie_detail_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/popular_movies_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/search_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/top_rated_movies_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/tv_series_detail_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/watchlist_movies_page.dart';
+import 'package:flutter_tv_series_app/presentation/provider/movie_list_notifier.dart';
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tv_series_app/presentation/provider/tv_series_list_notifier.dart';
+import 'package:provider/provider.dart';
+
+class HomeTvSeriesPage extends StatefulWidget {
+  static const ROUTE_NAME = '/home-tv';
+
+  const HomeTvSeriesPage({super.key});
+
+  @override
+  _HomeTvSeriesPageState createState() => _HomeTvSeriesPageState();
+}
+
+class _HomeTvSeriesPageState extends State<HomeTvSeriesPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => Provider.of<TvSeriesListNotifier>(context, listen: false)
+        ..fetchNowPlayingTvSeries()
+        ..fetchPopularTvSeries()
+        ..fetchTopRatedTvSeries(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/circle-g.png'),
+                backgroundColor: Colors.grey.shade900,
+              ),
+              accountName: Text('flutter_tv_series_app'),
+              accountEmail: Text('flutter_tv_series_app@dicoding.com'),
+              decoration: BoxDecoration(color: Colors.grey.shade900),
+            ),
+            ListTile(
+              leading: Icon(Icons.movie),
+              title: Text('Movies'),
+              onTap: () {
+                Navigator.pushNamed(context, HomeMoviePage.ROUTE_NAME);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.tv),
+              title: Text('TV Series'),
+              onTap: () {
+                Navigator.pushNamed(context, HomeTvSeriesPage.ROUTE_NAME);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.save_alt),
+              title: Text('Watchlist'),
+              onTap: () {
+                Navigator.pushNamed(context, WatchlistMoviesPage.ROUTE_NAME);
+              },
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pushNamed(context, AboutPage.ROUTE_NAME);
+              },
+              leading: Icon(Icons.info_outline),
+              title: Text('About'),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text('TV Series'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, SearchPage.ROUTE_NAME);
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Now Playing', style: kHeading6),
+              Consumer<TvSeriesListNotifier>(
+                builder: (context, data, child) {
+                  final state = data.nowPlayingState;
+                  if (state == RequestState.Loading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state == RequestState.Loaded) {
+                    return TvSeriesList(data.nowPlayingTvSeries);
+                  } else {
+                    return Text('Failed');
+                  }
+                },
+              ),
+              _buildSubHeading(
+                title: 'Popular',
+                onTap: () =>
+                    Navigator.pushNamed(context, PopularMoviesPage.ROUTE_NAME),
+              ),
+              Consumer<TvSeriesListNotifier>(
+                builder: (context, data, child) {
+                  final state = data.popularTvSeriesState;
+                  if (state == RequestState.Loading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state == RequestState.Loaded) {
+                    return TvSeriesList(data.popularTvSeries);
+                  } else {
+                    return Text('Failed');
+                  }
+                },
+              ),
+              _buildSubHeading(
+                title: 'Top Rated',
+                onTap: () =>
+                    Navigator.pushNamed(context, TopRatedMoviesPage.ROUTE_NAME),
+              ),
+              Consumer<TvSeriesListNotifier>(
+                builder: (context, data, child) {
+                  final state = data.topRatedTvSeriesState;
+                  if (state == RequestState.Loading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state == RequestState.Loaded) {
+                    return TvSeriesList(data.topRatedTvSeries);
+                  } else {
+                    return Text('Failed');
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row _buildSubHeading({required String title, required Function() onTap}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title, style: kHeading6),
+        InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [Text('See More'), Icon(Icons.arrow_forward_ios)],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TvSeriesList extends StatelessWidget {
+  final List<TvSeries> tvSeries;
+
+  const TvSeriesList(this.tvSeries, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final tv = tvSeries[index];
+          return Container(
+            padding: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(
+                  context,
+                  TvSeriesDetailPage.ROUTE_NAME,
+                  arguments: tv.id,
+                );
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+                child: CachedNetworkImage(
+                  imageUrl: '$BASE_IMAGE_URL${tv.posterPath}',
+                  placeholder: (context, url) =>
+                      Center(child: CircularProgressIndicator()),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: tvSeries.length,
+      ),
+    );
+  }
+}
