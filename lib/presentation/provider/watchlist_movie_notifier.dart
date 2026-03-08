6c09b67eab8ac1625a3cda68de1c0fed7783c093
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 16:51:13 +0800

    update import to flutter_tv_series_app

--- lib/presentation/provider/watchlist_movie_notifier.dart
+++ lib/presentation/provider/watchlist_movie_notifier.dart
@@ -1,6 +1,6 @@
-import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/domain/entities/movie.dart';
-import 'package:ditonton/domain/usecases/get_watchlist_movies.dart';
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_watchlist_movies.dart';
 import 'package:flutter/foundation.dart';
 
 class WatchlistMovieNotifier extends ChangeNotifier {
