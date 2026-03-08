581e17f327d109c091e8762012eac15c049e960e
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 16:50:15 +0800

    update import to flutter_tv_series_app

--- lib/presentation/pages/popular_movies_page.dart
+++ lib/presentation/pages/popular_movies_page.dart
@@ -1,6 +1,6 @@
-import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/presentation/provider/popular_movies_notifier.dart';
-import 'package:ditonton/presentation/widgets/movie_card_list.dart';
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter_tv_series_app/presentation/provider/popular_movies_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/widgets/movie_card_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,25 +15,24 @@ class _PopularMoviesPageState extends State<PopularMoviesPage> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(() =>
-        Provider.of<PopularMoviesNotifier>(context, listen: false)
-            .fetchPopularMovies());
+    Future.microtask(
+      () => Provider.of<PopularMoviesNotifier>(
+        context,
+        listen: false,
+      ).fetchPopularMovies(),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Popular Movies'),
-      ),
+      appBar: AppBar(title: Text('Popular Movies')),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Consumer<PopularMoviesNotifier>(
           builder: (context, data, child) {
             if (data.state == RequestState.Loading) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
+              return Center(child: CircularProgressIndicator());
             } else if (data.state == RequestState.Loaded) {
               return ListView.builder(
                 itemBuilder: (context, index) {
