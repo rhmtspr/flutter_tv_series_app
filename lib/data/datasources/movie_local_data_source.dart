fb91a1f0e940c0d83568278996d9e6f3e299cd44
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 16:45:54 +0800

    update import to flutter_tv_series_app

--- lib/data/datasources/movie_local_data_source.dart
+++ lib/data/datasources/movie_local_data_source.dart
@@ -1,6 +1,6 @@
-import 'package:ditonton/common/exception.dart';
-import 'package:ditonton/data/datasources/db/database_helper.dart';
-import 'package:ditonton/data/models/movie_table.dart';
+import 'package:flutter_tv_series_app/common/exception.dart';
+import 'package:flutter_tv_series_app/data/datasources/db/database_helper.dart';
+import 'package:flutter_tv_series_app/data/models/movie_table.dart';
 
 abstract class MovieLocalDataSource {
   Future<String> insertWatchlist(MovieTable movie);
