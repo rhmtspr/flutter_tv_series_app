928df4a76a82838ce715b73fde6b4da073cd63ca
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-09 00:33:27 +0800

    create remove watchlist tv series usecase

---
+++ lib/domain/usecases/remove_watchlist_tv_series.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_tv_series_app/common/failure.dart';
+import 'package:flutter_tv_series_app/domain/entities/tv_series_detail.dart';
+import 'package:flutter_tv_series_app/domain/repositories/tv_series_repository.dart';
+
+class RemoveWatchlistTvSeries {
+  final TvSeriesRepository repository;
+
+  RemoveWatchlistTvSeries(this.repository);
+
+  Future<Either<Failure, String>> execute(TvSeriesDetail tv) {
+    return repository.removeWatchlistTvSeries(tv);
+  }
+}
