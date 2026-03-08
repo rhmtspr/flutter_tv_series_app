36edcb95dc71bcdded4ba71e53c009d8395a687a
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 16:45:47 +0800

    update import to flutter_tv_series_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
-import 'package:ditonton/common/constants.dart';
-import 'package:ditonton/common/utils.dart';
-import 'package:ditonton/presentation/pages/about_page.dart';
-import 'package:ditonton/presentation/pages/movie_detail_page.dart';
-import 'package:ditonton/presentation/pages/home_movie_page.dart';
-import 'package:ditonton/presentation/pages/popular_movies_page.dart';
-import 'package:ditonton/presentation/pages/search_page.dart';
-import 'package:ditonton/presentation/pages/top_rated_movies_page.dart';
-import 'package:ditonton/presentation/pages/watchlist_movies_page.dart';
-import 'package:ditonton/presentation/provider/movie_detail_notifier.dart';
-import 'package:ditonton/presentation/provider/movie_list_notifier.dart';
-import 'package:ditonton/presentation/provider/movie_search_notifier.dart';
-import 'package:ditonton/presentation/provider/popular_movies_notifier.dart';
-import 'package:ditonton/presentation/provider/top_rated_movies_notifier.dart';
-import 'package:ditonton/presentation/provider/watchlist_movie_notifier.dart';
+import 'package:flutter_tv_series_app/common/constants.dart';
+import 'package:flutter_tv_series_app/common/utils.dart';
+import 'package:flutter_tv_series_app/presentation/pages/about_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/home_tv_series_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/movie_detail_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/home_movie_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/popular_movies_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/search_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/top_rated_movies_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/watchlist_movies_page.dart';
+import 'package:flutter_tv_series_app/presentation/provider/movie_detail_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/movie_list_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/movie_search_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/popular_movies_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/top_rated_movies_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:ditonton/injection.dart' as di;
+import 'package:flutter_tv_series_app/injection.dart' as di;
 
 void main() {
   di.init();
@@ -24,13 +25,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (_) => di.locator<MovieListNotifier>(),
-        ),
+        ChangeNotifierProvider(create: (_) => di.locator<MovieListNotifier>()),
         ChangeNotifierProvider(
           create: (_) => di.locator<MovieDetailNotifier>(),
         ),
@@ -62,6 +63,8 @@ class MyApp extends StatelessWidget {
           switch (settings.name) {
             case '/home':
               return MaterialPageRoute(builder: (_) => HomeMoviePage());
+            case HomeTVSeriesPage.ROUTE_NAME:
+              return MaterialPageRoute(builder: (_) => HomeTVSeriesPage());
             case PopularMoviesPage.ROUTE_NAME:
               return CupertinoPageRoute(builder: (_) => PopularMoviesPage());
             case TopRatedMoviesPage.ROUTE_NAME:
@@ -79,13 +82,13 @@ class MyApp extends StatelessWidget {
             case AboutPage.ROUTE_NAME:
               return MaterialPageRoute(builder: (_) => AboutPage());
             default:
-              return MaterialPageRoute(builder: (_) {
-                return Scaffold(
-                  body: Center(
-                    child: Text('Page not found :('),
-                  ),
-                );
-              });
+              return MaterialPageRoute(
+                builder: (_) {
+                  return Scaffold(
+                    body: Center(child: Text('Page not found :(')),
+                  );
+                },
+              );
           }
         },
       ),
