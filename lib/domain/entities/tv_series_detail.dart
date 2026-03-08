a23a823924b297b624ce618c805809156621abed
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-09 00:30:00 +0800

    create tv series detail

---
+++ lib/domain/entities/tv_series_detail.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_tv_series_app/domain/entities/genre.dart';
+import 'package:equatable/equatable.dart';
+
+class TvSeriesDetail extends Equatable {
+  TvSeriesDetail({
+    required this.adult,
+    required this.backdropPath,
+    required this.genres,
+    required this.id,
+    required this.name,
+    required this.overview,
+    required this.posterPath,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final bool adult;
+  final String? backdropPath;
+  final List<Genre> genres;
+  final int id;
+  final String name;
+  final String overview;
+  final String posterPath;
+  final double voteAverage;
+  final int voteCount;
+
+  @override
+  List<Object?> get props => [
+    adult,
+    backdropPath,
+    genres,
+    id,
+    overview,
+    posterPath,
+    voteAverage,
+    voteCount,
+  ];
+}
