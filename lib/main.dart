9fa7ce0dfbbafa43b2b67982e643200ab1671c8f
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-09 00:27:21 +0800

    add tv series notifier, top rated and popular

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,19 @@ import 'package:flutter_tv_series_app/presentation/pages/home_tv_series_page.dar
 import 'package:flutter_tv_series_app/presentation/pages/movie_detail_page.dart';
 import 'package:flutter_tv_series_app/presentation/pages/home_movie_page.dart';
 import 'package:flutter_tv_series_app/presentation/pages/popular_movies_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/popular_tv_series_page.dart';
 import 'package:flutter_tv_series_app/presentation/pages/search_page.dart';
 import 'package:flutter_tv_series_app/presentation/pages/top_rated_movies_page.dart';
+import 'package:flutter_tv_series_app/presentation/pages/top_rated_tv_series_page.dart';
 import 'package:flutter_tv_series_app/presentation/pages/watchlist_movies_page.dart';
 import 'package:flutter_tv_series_app/presentation/provider/movie_detail_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/movie_list_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/movie_search_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/popular_movies_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/popular_tv_series_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/top_rated_movies_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/top_rated_tv_series.dart';
+import 'package:flutter_tv_series_app/presentation/provider/tv_series_list_notifier.dart';
 import 'package:flutter_tv_series_app/presentation/provider/watchlist_movie_notifier.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -31,6 +36,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        // ========================
+        // Movies Provider
         ChangeNotifierProvider(create: (_) => di.locator<MovieListNotifier>()),
         ChangeNotifierProvider(
           create: (_) => di.locator<MovieDetailNotifier>(),
@@ -46,6 +53,18 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => di.locator<WatchlistMovieNotifier>(),
+        ),
+
+        // ========================
+        // Tv Series Provider
+        ChangeNotifierProvider(
+          create: (_) => di.locator<TvSeriesListNotifier>(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => di.locator<TopRatedTvSeriesNotifier>(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => di.locator<PopularTvSeriesNotifier>(),
         ),
       ],
       child: MaterialApp(
@@ -63,8 +82,6 @@ class MyApp extends StatelessWidget {
           switch (settings.name) {
             case '/home':
               return MaterialPageRoute(builder: (_) => HomeMoviePage());
-            case HomeTVSeriesPage.ROUTE_NAME:
-              return MaterialPageRoute(builder: (_) => HomeTVSeriesPage());
             case PopularMoviesPage.ROUTE_NAME:
               return CupertinoPageRoute(builder: (_) => PopularMoviesPage());
             case TopRatedMoviesPage.ROUTE_NAME:
@@ -81,6 +98,14 @@ class MyApp extends StatelessWidget {
               return MaterialPageRoute(builder: (_) => WatchlistMoviesPage());
             case AboutPage.ROUTE_NAME:
               return MaterialPageRoute(builder: (_) => AboutPage());
+
+            // Tv Series
+            case HomeTvSeriesPage.ROUTE_NAME:
+              return MaterialPageRoute(builder: (_) => HomeTvSeriesPage());
+            case PopularTvSeriesPage.ROUTE_NAME:
+              return CupertinoPageRoute(builder: (_) => PopularTvSeriesPage());
+            case TopRatedTvSeriesPage.ROUTE_NAME:
+              return CupertinoPageRoute(builder: (_) => TopRatedTvSeriesPage());
             default:
               return MaterialPageRoute(
                 builder: (_) {
