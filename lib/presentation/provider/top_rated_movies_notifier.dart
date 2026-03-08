4e0ec220b72aaee10087451138fa06299392c735
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 16:50:50 +0800

    update import to flutter_tv_series_app

--- lib/presentation/provider/top_rated_movies_notifier.dart
+++ lib/presentation/provider/top_rated_movies_notifier.dart
@@ -1,6 +1,6 @@
-import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/domain/entities/movie.dart';
-import 'package:ditonton/domain/usecases/get_top_rated_movies.dart';
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_top_rated_movies.dart';
 import 'package:flutter/foundation.dart';
 
 class TopRatedMoviesNotifier extends ChangeNotifier {
