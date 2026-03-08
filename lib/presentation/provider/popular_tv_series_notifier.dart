e9bfc793cd1dfd8d668c758fd30bb7cf5c5081de
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-09 00:36:18 +0800

    create popular tv seires notifier

---
+++ lib/presentation/provider/popular_tv_series_notifier.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie.dart';
+import 'package:flutter_tv_series_app/domain/entities/tv_series.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_popular_movies.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_popular_tv_series.dart';
+
+class PopularTvSeriesNotifier extends ChangeNotifier {
+  final GetPopularTvSeries getPopularTvSeries;
+
+  PopularTvSeriesNotifier(this.getPopularTvSeries);
+
+  RequestState _state = RequestState.Empty;
+  RequestState get state => _state;
+
+  List<TvSeries> _tvSeries = [];
+  List<TvSeries> get tvSeries => _tvSeries;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchPopularMovies() async {
+    _state = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getPopularTvSeries.execute();
+
+    result.fold(
+      (failure) {
+        _message = failure.message;
+        _state = RequestState.Error;
+        notifyListeners();
+      },
+      (tvSeriesData) {
+        _tvSeries = tvSeriesData;
+        _state = RequestState.Loaded;
+        notifyListeners();
+      },
+    );
+  }
+}
