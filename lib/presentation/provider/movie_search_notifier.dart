02af554dab2c42ddb8ef811c72438b223a163356
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 16:50:58 +0800

    update import to flutter_tv_series_app

--- lib/presentation/provider/movie_search_notifier.dart
+++ lib/presentation/provider/movie_search_notifier.dart
@@ -1,6 +1,6 @@
-import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/domain/entities/movie.dart';
-import 'package:ditonton/domain/usecases/search_movies.dart';
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie.dart';
+import 'package:flutter_tv_series_app/domain/usecases/search_movies.dart';
 import 'package:flutter/foundation.dart';
 
 class MovieSearchNotifier extends ChangeNotifier {
