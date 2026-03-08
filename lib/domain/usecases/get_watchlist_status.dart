a0bfec3aeba493802e16ced0f55b62c08315d683
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 16:49:44 +0800

    update import to flutter_tv_series_app

--- lib/domain/usecases/get_watchlist_status.dart
+++ lib/domain/usecases/get_watchlist_status.dart
@@ -1,4 +1,4 @@
-import 'package:ditonton/domain/repositories/movie_repository.dart';
+import 'package:flutter_tv_series_app/domain/repositories/movie_repository.dart';
 
 class GetWatchListStatus {
   final MovieRepository repository;
