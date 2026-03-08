d30f4e3757814715b4806e32ef256a2337aa3f1d
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-08 16:48:11 +0800

    update import to flutter_tv_series_app

--- lib/data/repositories/movie_repository_impl.dart
+++ lib/data/repositories/movie_repository_impl.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
-import 'package:ditonton/data/datasources/movie_local_data_source.dart';
-import 'package:ditonton/data/datasources/movie_remote_data_source.dart';
-import 'package:ditonton/data/models/movie_table.dart';
-import 'package:ditonton/domain/entities/movie.dart';
-import 'package:ditonton/domain/entities/movie_detail.dart';
-import 'package:ditonton/domain/repositories/movie_repository.dart';
-import 'package:ditonton/common/exception.dart';
-import 'package:ditonton/common/failure.dart';
+import 'package:flutter_tv_series_app/data/datasources/movie_local_data_source.dart';
+import 'package:flutter_tv_series_app/data/datasources/movie_remote_data_source.dart';
+import 'package:flutter_tv_series_app/data/models/movie_table.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie_detail.dart';
+import 'package:flutter_tv_series_app/domain/repositories/movie_repository.dart';
+import 'package:flutter_tv_series_app/common/exception.dart';
+import 'package:flutter_tv_series_app/common/failure.dart';
 
 class MovieRepositoryImpl implements MovieRepository {
   final MovieRemoteDataSource remoteDataSource;
@@ -94,8 +94,9 @@ class MovieRepositoryImpl implements MovieRepository {
   @override
   Future<Either<Failure, String>> saveWatchlist(MovieDetail movie) async {
     try {
-      final result =
-          await localDataSource.insertWatchlist(MovieTable.fromEntity(movie));
+      final result = await localDataSource.insertWatchlist(
+        MovieTable.fromEntity(movie),
+      );
       return Right(result);
     } on DatabaseException catch (e) {
       return Left(DatabaseFailure(e.message));
@@ -107,8 +108,9 @@ class MovieRepositoryImpl implements MovieRepository {
   @override
   Future<Either<Failure, String>> removeWatchlist(MovieDetail movie) async {
     try {
-      final result =
-          await localDataSource.removeWatchlist(MovieTable.fromEntity(movie));
+      final result = await localDataSource.removeWatchlist(
+        MovieTable.fromEntity(movie),
+      );
       return Right(result);
     } on DatabaseException catch (e) {
       return Left(DatabaseFailure(e.message));
