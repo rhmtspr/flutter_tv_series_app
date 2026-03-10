df60ea61e897dac72c013028070a255fd31e8b18
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-10 21:44:03 +0800

    add tv series dummy object

--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -2,7 +2,14 @@ import 'package:flutter_tv_series_app/data/models/movie_table.dart';
 import 'package:flutter_tv_series_app/domain/entities/genre.dart';
 import 'package:flutter_tv_series_app/domain/entities/movie.dart';
 import 'package:flutter_tv_series_app/domain/entities/movie_detail.dart';
+import 'package:flutter_tv_series_app/data/models/tv_series_table.dart';
+import 'package:flutter_tv_series_app/domain/entities/genre.dart';
+import 'package:flutter_tv_series_app/domain/entities/tv_series.dart';
+import 'package:flutter_tv_series_app/domain/entities/tv_series_detail.dart';
 
+// ============================================
+// Movies
+// ============================================
 final testMovie = Movie(
   adult: false,
   backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
@@ -80,4 +87,83 @@ final testMovieMap = {
   'overview': 'overview',
   'posterPath': 'posterPath',
   'title': 'title',
+};
+
+// ============================================
+// TV Series
+// ============================================
+final testTvSeries = TvSeries(
+  backdropPath: '/mAJ84W6I8I272Da87qplS2Dp9ST.jpg',
+  firstAirDate: '2023-01-23',
+  genreIds: [14, 28],
+  id: 202250,
+  name: 'Dirty Linen',
+  originCountry: ['PH'],
+  originalLanguage: 'tl',
+  originalName: 'Dirty Linen',
+  overview:
+      'To exact vengeance, a young woman infiltrates the household of an influential family as a housemaid to expose their dirty secrets. However, love will get in the way of her revenge plot.',
+  popularity: 2797.914,
+  posterPath: '/aoAZgnmMzY9vVy9VWnO3U5PZENh.jpg',
+  voteAverage: 5,
+  voteCount: 13,
+);
+
+final testTvSeriesList = [testTvSeries];
+
+final testTvSeriesDetail = TvSeriesDetail(
+  adult: false,
+  backdropPath: 'backdropPath',
+  genres: [Genre(id: 1, name: 'Action')],
+  id: 1,
+  name: 'name',
+  overview: 'overview',
+  posterPath: 'posterPath',
+  voteAverage: 1,
+  voteCount: 1,
+);
+
+final testTvSeriesCache = TvSeriesTable(
+  id: 202250,
+  overview:
+      'To exact vengeance, a young woman infiltrates the household of an influential family as a housemaid to expose their dirty secrets. However, love will get in the way of her revenge plot.',
+  posterPath: '/mAJ84W6I8I272Da87qplS2Dp9ST.jpg',
+  name: 'Dirty Linen',
+);
+
+final testTvSeriesCacheMap = {
+  'id': 202250,
+  'overview':
+      'To exact vengeance, a young woman infiltrates the household of an influential family as a housemaid to expose their dirty secrets. However, love will get in the way of her revenge plot.',
+  'posterPath': '/mAJ84W6I8I272Da87qplS2Dp9ST.jpg',
+  'name': 'Dirty Linen',
+};
+
+final testTvSeriesFromCache = TvSeries.watchlist(
+  id: 202250,
+  overview:
+      'To exact vengeance, a young woman infiltrates the household of an influential family as a housemaid to expose their dirty secrets. However, love will get in the way of her revenge plot.',
+  posterPath: '/mAJ84W6I8I272Da87qplS2Dp9ST.jpg',
+  name: 'Dirty Linen',
+);
+
+final testWatchlistTvSeries = TvSeries.watchlist(
+  id: 1,
+  name: 'name',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testTvSeriesTable = TvSeriesTable(
+  id: 1,
+  name: 'name',
+  posterPath: 'posterPath',
+  overview: 'overview',
+);
+
+final testTvSeriesMap = {
+  'id': 1,
+  'overview': 'overview',
+  'posterPath': 'posterPath',
+  'name': 'name',
 };
