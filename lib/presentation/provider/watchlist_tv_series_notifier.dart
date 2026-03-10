894fdc99bcb1921e6f83d86fd4363b1e6ace4c59
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-10 21:16:42 +0800

    create watchlist_tv_series_notifier.dart

---
+++ lib/presentation/provider/watchlist_tv_series_notifier.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter_tv_series_app/domain/entities/tv_series.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_watchlist_tv_series.dart';
+
+class WatchlistTvSeriesNotifier extends ChangeNotifier {
+  var _watchlistTvSeries = <TvSeries>[];
+  List<TvSeries> get watchlistTvSeries => _watchlistTvSeries;
+
+  var _watchlistState = RequestState.Empty;
+  RequestState get watchlistState => _watchlistState;
+
+  String _message = '';
+  String get message => _message;
+
+  WatchlistTvSeriesNotifier({required this.getWatchlistTvSeries});
+
+  final GetWatchlistTvSeries getWatchlistTvSeries;
+
+  Future<void> fetchWatchlistTvSeries() async {
+    _watchlistState = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getWatchlistTvSeries.execute();
+    print(result);
+    result.fold(
+      (failure) {
+        _watchlistState = RequestState.Error;
+        _message = failure.message;
+        notifyListeners();
+      },
+      (tvSeriesData) {
+        _watchlistState = RequestState.Loaded;
+        _watchlistTvSeries = tvSeriesData;
+        notifyListeners();
+      },
+    );
+  }
+}
