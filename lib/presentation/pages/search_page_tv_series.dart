87562c4d624cbc6cde3a7713d4361bbb2cc5ffc4
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-10 21:13:51 +0800

    create search_page_tv_series.dart

---
+++ lib/presentation/pages/search_page_tv_series.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_tv_series_app/common/constants.dart';
+import 'package:flutter_tv_series_app/common/state_enum.dart';
+import 'package:flutter_tv_series_app/presentation/provider/movie_search_notifier.dart';
+import 'package:flutter_tv_series_app/presentation/provider/tv_series_search_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tv_series_app/presentation/widgets/tv_series_card_list.dart';
+import 'package:provider/provider.dart';
+
+class SearchPageTvSeries extends StatelessWidget {
+  static const ROUTE_NAME = '/search-tv-series';
+
+  const SearchPageTvSeries({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Search')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              onSubmitted: (query) {
+                Provider.of<MovieSearchNotifier>(
+                  context,
+                  listen: false,
+                ).fetchMovieSearch(query);
+              },
+              decoration: InputDecoration(
+                hintText: 'Search title',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              textInputAction: TextInputAction.search,
+            ),
+            SizedBox(height: 16),
+            Text('Search Result', style: kHeading6),
+            Consumer<TvSeriesSearchNotifier>(
+              builder: (context, data, child) {
+                if (data.state == RequestState.Loading) {
+                  return Center(child: CircularProgressIndicator());
+                } else if (data.state == RequestState.Loaded) {
+                  final result = data.searchResult;
+                  return Expanded(
+                    child: ListView.builder(
+                      padding: const EdgeInsets.all(8),
+                      itemBuilder: (context, index) {
+                        final tv = data.searchResult[index];
+                        return TvSeriesCard(tv);
+                      },
+                      itemCount: result.length,
+                    ),
+                  );
+                } else {
+                  return Expanded(child: Container());
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
