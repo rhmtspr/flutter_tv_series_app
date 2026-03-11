362ad0b57d50150fddd76791d1c26c9aef8ba6bb
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-12 00:55:43 +0800

    change Now playing to now playing and make if statement with isNotEmpy

--- lib/data/datasources/movie_local_data_source.dart
+++ lib/data/datasources/movie_local_data_source.dart
@@ -54,14 +54,14 @@ class MovieLocalDataSourceImpl implements MovieLocalDataSource {
 
   @override
   Future<void> cacheNowPlayingMovies(List<MovieTable> movies) async {
-    await databaseHelper.clearCacheMovies('Now playing');
+    await databaseHelper.clearCacheMovies('now playing');
     await databaseHelper.insertCacheTransactionMovie(movies, 'now playing');
   }
 
   @override
   Future<List<MovieTable>> getCachedNowPlayingMovies() async {
     final result = await databaseHelper.getCacheMovies('now playing');
-    if (result.length > 0) {
+    if (result.isNotEmpty) {
       return result.map((data) => MovieTable.fromMap(data)).toList();
     } else {
       throw CacheException("Can't get the data:(");
