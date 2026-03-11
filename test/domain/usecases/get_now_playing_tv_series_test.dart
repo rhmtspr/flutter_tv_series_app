ffa275c21ca99b43e6f26aeae2bd9f378bcf50cc
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-12 01:03:41 +0800

    add get_now_playing_tv_series_test.dart

---
+++ test/domain/usecases/get_now_playing_tv_series_test.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_tv_series_app/domain/entities/tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_now_playing_tv_series.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetNowPlayingTvSeries usecase;
+  late MockTvSeriesRepository mockTvSeriesRepository;
+
+  setUp(() {
+    mockTvSeriesRepository = MockTvSeriesRepository();
+    usecase = GetNowPlayingTvSeries(mockTvSeriesRepository);
+  });
+
+  final tTvSeries = <TvSeries>[];
+
+  test('should get list of TV Series from the repository', () async {
+    // arrange
+    when(
+      mockTvSeriesRepository.getNowPlayingTvSeries(),
+    ).thenAnswer((_) async => Right(tTvSeries));
+    // act
+    final result = await usecase.execute();
+    // assert
+    expect(result, Right(tTvSeries));
+  });
+}
