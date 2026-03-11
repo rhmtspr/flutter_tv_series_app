ca621a3884003c0d3874b167876672842de5a730
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-11 11:46:45 +0800

    add test for movie usecases

---
+++ test/domain/usecases/get_watchlist_status_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_watchlist_status_movie.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetWatchListStatusMovie usecase;
+  late MockMovieRepository mockMovieRepository;
+
+  setUp(() {
+    mockMovieRepository = MockMovieRepository();
+    usecase = GetWatchListStatusMovie(mockMovieRepository);
+  });
+
+  test('should get watchlist status from repository', () async {
+    // arrange
+    when(
+      mockMovieRepository.isAddedToWatchlistMovie(1),
+    ).thenAnswer((_) async => true);
+    // act
+    final result = await usecase.execute(1);
+    // assert
+    expect(result, true);
+  });
+}
---
+++ test/domain/usecases/save_watchlist_test.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_tv_series_app/domain/usecases/save_watchlist_movie.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../dummy_data/dummy_objects.dart';
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late SaveWatchlistMovie usecase;
+  late MockMovieRepository mockMovieRepository;
+
+  setUp(() {
+    mockMovieRepository = MockMovieRepository();
+    usecase = SaveWatchlistMovie(mockMovieRepository);
+  });
+
+  test('should save movie to the repository', () async {
+    // arrange
+    when(
+      mockMovieRepository.saveWatchlistMovie(testMovieDetail),
+    ).thenAnswer((_) async => Right('Added to Watchlist'));
+    // act
+    final result = await usecase.execute(testMovieDetail);
+    // assert
+    verify(mockMovieRepository.saveWatchlistMovie(testMovieDetail));
+    expect(result, Right('Added to Watchlist'));
+  });
+}
