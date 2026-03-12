2db6b31cfd9dd6b69c7352f5ff1ff9829aaea48c
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-13 00:22:05 +0800

    add loadingState

--- lib/presentation/provider/popular_movies_notifier.dart
+++ lib/presentation/provider/popular_movies_notifier.dart
@@ -8,7 +8,7 @@ class PopularMoviesNotifier extends ChangeNotifier {
 
   PopularMoviesNotifier(this.getPopularMovies);
 
-  RequestState _state = RequestState.Empty;
+  RequestState _state = RequestState.emptyState;
   RequestState get state => _state;
 
   List<Movie> _movies = [];
@@ -18,7 +18,7 @@ class PopularMoviesNotifier extends ChangeNotifier {
   String get message => _message;
 
   Future<void> fetchPopularMovies() async {
-    _state = RequestState.Loading;
+    _state = RequestState.loadingState;
     notifyListeners();
 
     final result = await getPopularMovies.execute();
@@ -26,12 +26,12 @@ class PopularMoviesNotifier extends ChangeNotifier {
     result.fold(
       (failure) {
         _message = failure.message;
-        _state = RequestState.Error;
+        _state = RequestState.errorState;
         notifyListeners();
       },
       (moviesData) {
         _movies = moviesData;
-        _state = RequestState.Loaded;
+        _state = RequestState.loadedState;
         notifyListeners();
       },
     );
