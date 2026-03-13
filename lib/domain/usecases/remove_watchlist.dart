89d4abb484335b5bdd07f8f0d3199aec9757e038
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-14 02:52:56 +0800

    add s at movie

--- lib/domain/usecases/remove_watchlist.dart
+++ lib/domain/usecases/remove_watchlist.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_tv_series_app/common/failure.dart';
-import 'package:flutter_tv_series_app/domain/entities/movie_detail.dart';
-import 'package:flutter_tv_series_app/domain/repositories/movie_repository.dart';
+import 'package:flutter_tv_series_app/domain/entities/movies_detail.dart';
+import 'package:flutter_tv_series_app/domain/repositories/movies_repository.dart';
 
 class RemoveWatchlistMovie {
   final MovieRepository repository;
