f5e5a54d48d118bbeae727ac15375b17f3b7f085
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-13 20:34:32 +0800

    create popular_movies_bloc.dart

---
+++ lib/presentation/bloc/popular_movies_bloc.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_tv_series_app/domain/entities/movie.dart';
+import 'package:flutter_tv_series_app/domain/usecases/get_popular_movies.dart';
+
+part 'popular_movies_event.dart';
+part 'popular_movies_state.dart';
+
+class PopularMoviesBloc extends Bloc<PopularMoviesEvent, PopularMoviesState> {
+  final GetPopularMovies getPopularMovies;
+
+  PopularMoviesBloc(this.getPopularMovies) : super(PopularMoviesEmpty()) {
+    on<FetchPopularMovies>((event, emit) async {
+      emit(PopularMoviesLoading());
+      final result = await getPopularMovies.execute();
+
+      result.fold(
+        (failure) {
+          emit(PopularMoviesError(failure.message));
+        },
+        (data) {
+          emit(PopularMoviesLoaded(data));
+        },
+      );
+    });
+  }
+}
