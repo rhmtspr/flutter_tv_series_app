29b33d9e7575fdb31bad22b82509c388bffbe36b
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-13 21:28:10 +0800

    migrate to BlocBuilder

--- lib/presentation/pages/popular_movies_page.dart
+++ lib/presentation/pages/popular_movies_page.dart
@@ -1,8 +1,7 @@
-import 'package:flutter_tv_series_app/common/state_enum.dart';
-import 'package:flutter_tv_series_app/presentation/provider/popular_movies_notifier.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_tv_series_app/presentation/bloc/popular_movies_bloc.dart';
 import 'package:flutter_tv_series_app/presentation/widgets/movie_card_list.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class PopularMoviesPage extends StatefulWidget {
   static const routeName = '/popular-movie';
@@ -17,12 +16,11 @@ class PopularMoviesPageState extends State<PopularMoviesPage> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(
-      () => Provider.of<PopularMoviesNotifier>(
-        context,
-        listen: false,
-      ).fetchPopularMovies(),
-    );
+    Future.microtask(() {
+      if (!mounted) return;
+
+      context.read<PopularMoviesBloc>().add(FetchPopularMovies());
+    });
   }
 
   @override
@@ -31,23 +29,25 @@ class PopularMoviesPageState extends State<PopularMoviesPage> {
       appBar: AppBar(title: Text('Popular Movies')),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Consumer<PopularMoviesNotifier>(
-          builder: (context, data, child) {
-            if (data.state == RequestState.loadingState) {
+        child: BlocBuilder<PopularMoviesBloc, PopularMoviesState>(
+          builder: (context, state) {
+            if (state is PopularMoviesLoading) {
               return Center(child: CircularProgressIndicator());
-            } else if (data.state == RequestState.loadedState) {
+            } else if (state is PopularMoviesLoaded) {
               return ListView.builder(
                 itemBuilder: (context, index) {
-                  final movie = data.movies[index];
+                  final movie = state.movies[index];
                   return MovieCard(movie);
                 },
-                itemCount: data.movies.length,
+                itemCount: state.movies.length,
               );
-            } else {
+            } else if (state is PopularMoviesError) {
               return Center(
                 key: Key('error_message'),
-                child: Text(data.message),
+                child: Text(state.message),
               );
+            } else {
+              return Center();
             }
           },
         ),
