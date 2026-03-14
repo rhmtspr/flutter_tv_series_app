c5e455887e55b3531fd360f567addb61200ba45b
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-14 23:39:56 +0800

    migrate to bloc

--- lib/presentation/pages/popular_tv_series_page.dart
+++ lib/presentation/pages/popular_tv_series_page.dart
@@ -1,8 +1,7 @@
-import 'package:flutter_tv_series_app/common/state_enum.dart';
-import 'package:flutter_tv_series_app/presentation/provider/popular_tv_series_notifier.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_tv_series_app/presentation/bloc/popular_tv_series_bloc.dart';
 import 'package:flutter_tv_series_app/presentation/widgets/tv_series_card_list.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
 
 class PopularTvSeriesPage extends StatefulWidget {
   static const routeName = '/popular-tv-series';
@@ -17,37 +16,38 @@ class PopularTvSeriesPageState extends State<PopularTvSeriesPage> {
   @override
   void initState() {
     super.initState();
-    Future.microtask(
-      () => Provider.of<PopularTvSeriesNotifier>(
-        context,
-        listen: false,
-      ).fetchPopularTvSeries(),
-    );
+    Future.microtask(() {
+      if (!mounted) return;
+
+      context.read<PopularTvSeriesBloc>().add(FetchPopularTvSeries());
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Popular TV Series')),
+      appBar: AppBar(title: Text('Popular TvSeries')),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Consumer<PopularTvSeriesNotifier>(
-          builder: (context, data, child) {
-            if (data.state == RequestState.loadingState) {
+        child: BlocBuilder<PopularTvSeriesBloc, PopularTvSeriesState>(
+          builder: (context, state) {
+            if (state is PopularTvSeriesLoading) {
               return Center(child: CircularProgressIndicator());
-            } else if (data.state == RequestState.loadedState) {
+            } else if (state is PopularTvSeriesLoaded) {
               return ListView.builder(
                 itemBuilder: (context, index) {
-                  final tv = data.tvSeries[index];
+                  final tv = state.tvSeries[index];
                   return TvSeriesCard(tv);
                 },
-                itemCount: data.tvSeries.length,
+                itemCount: state.tvSeries.length,
               );
-            } else {
+            } else if (state is PopularTvSeriesError) {
               return Center(
                 key: Key('error_message'),
-                child: Text(data.message),
+                child: Text(state.message),
               );
+            } else {
+              return Center();
             }
           },
         ),
